97bf8822404a0c0ac28e3844e958677800641b62
Mikhail <uiqwevas7@gmail.com>
2025-06-11 23:58:45 +0300

    Add files via upload

---
+++ error-js.js
@@ -0,0 +1,21 @@
+const header = document.querySelector('header');
+
+const labelSvg = document.querySelector('.label svg');
+labelSvg.addEventListener('click', function () {
+  location.reload()
+})
+
+const menuA = document.querySelectorAll('.menu a');
+menuA.forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location.href = link.dataset.href;
+  })
+})
+
+
+const onTheMain = document.querySelector('.onTheMain');
+onTheMain.addEventListener('click', (e) => {
+  e.preventDefault();
+  window.location.href = onTheMain.dataset.href;
+})
